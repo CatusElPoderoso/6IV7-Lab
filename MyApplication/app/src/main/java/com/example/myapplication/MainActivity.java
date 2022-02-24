35196c71c324b738da6e6eb0d0fb107cce18168b
Ariadna <achicol1900@alumno.ipn.mx>
2022-02-24 00:32:03 -0600

    este es el bueno

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,4 +1,4 @@
-  package com.example.myapplication;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,49 +8,42 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.IOException;
+public class MainActivity extends AppCompatActivity {
 
-  public class MainActivity extends AppCompatActivity {
-
-      float promedio=0, promedioSemestre=0, calificacion=0,C;
-      TextView labelA;
-      TextView labelP;
-      TextView labelParcial;
-      EditText textField;
-      int acumulacion=0, parcial=1;
-
+    TextView promedioparcial,titulo;
+    EditText uno,dos,tres,cuatro,cinco,seis,siete,ocho,nueve,diez;
+    float promedio=0, sumatoria=0,x=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        labelA = findViewById(R.id.textViewAcumuladas);
-        labelP = findViewById(R.id.textViewPromedio);
-        labelParcial = findViewById(R.id.textViewParcial);
-        textField=findViewById(R.id.editTextC);
+        uno= findViewById(R.id.editTextTextPersonName);
+        dos= findViewById(R.id.editTextTextPersonName2);
+        tres= findViewById(R.id.editTextTextPersonName3);
+        cuatro= findViewById(R.id.editTextTextPersonName4);
+        cinco=findViewById(R.id.editTextTextPersonName5);
+        seis=findViewById(R.id.editTextTextPersonName6);
+        siete=findViewById(R.id.editTextTextPersonName7);
+        ocho=findViewById(R.id.editTextTextPersonName8);
+        nueve=findViewById(R.id.editTextTextPersonName9);
+        diez=findViewById(R.id.editTextTextPersonName10);
+        promedioparcial =findViewById(R.id.textViewR);
+        titulo=findViewById(R.id.titulo);
     }
-    public void Sumar (View view){
+    public void Siguiente(View view){
         try {
-            acumulacion = acumulacion + 1;
-            promedio = (promedio + Float.parseFloat(textField.getText().toString()))/acumulacion;
-            labelA.setText(String.valueOf(acumulacion));
-            labelP.setText(String.valueOf(promedio));
-            textField.setText("");
+
+            sumatoria = (Float.parseFloat(uno.getText().toString())) + (Float.parseFloat(dos.getText().toString())) + (Float.parseFloat(tres.getText().toString())) + (Float.parseFloat(cuatro.getText().toString())) + (Float.parseFloat(cinco.getText().toString())) + (Float.parseFloat(seis.getText().toString())) + (Float.parseFloat(siete.getText().toString())) + (Float.parseFloat(ocho.getText().toString())) + (Float.parseFloat(nueve.getText().toString())) + (Float.parseFloat(diez.getText().toString()));
+            promedioparcial.setText(String.valueOf(sumatoria / 10));
+            uno.setText("");dos.setText("");tres.setText("");cuatro.setText("");cinco.setText("");seis.setText("");siete.setText("");ocho.setText("");nueve.setText("");diez.setText("");
+            x++;
+            if (x<4){
+            titulo.setText("Parcial "+x);
+            promedio=promedio+(sumatoria/10);
+            }else {Toast.makeText(getApplicationContext(), "El promedio semestral es: "+promedio/3, Toast.LENGTH_LONG).show(); }
         }catch (Exception ex){
-            Toast.makeText(getApplicationContext(), "Ingrese numeros, no letras", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "Ingrese los datos de forma correcta", Toast.LENGTH_LONG).show();
         }
     }
 
-    public void siguienteSemestre(View view){
-        acumulacion=0;
-        labelA.setText(String.valueOf(acumulacion));
-        promedioSemestre=promedioSemestre+promedio;
-        parcial=parcial+1;
-        promedio=0;
-        labelP.setText(String.valueOf(promedio));
-        labelParcial.setText("Parcial "+parcial);
-        if (parcial==4){
-            C=promedioSemestre/(parcial-1);
-            Toast.makeText(getApplicationContext(), "El promedio semestral es: "+C, Toast.LENGTH_SHORT).show();
-        }
-    }
 }
